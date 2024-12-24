2da850b768b8dbb59e71584b818f49a39aeb6955
jeyakumar-krish <jeyakumarjk032@gamil.com>
2024-12-24 23:51:22 +0530

    Testing1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const layout = ({ children } : {children: React.ReactNode}) => {
   return (
     <div>
-        <h1>Root</h1>
+        <h1>I am The Root</h1>
         {children}
     </div>
   )
